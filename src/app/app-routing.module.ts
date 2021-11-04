67623d5b74130aace16e9947807f9daec04abb24
beaavelino <beatrizavelino0088@gmail.com>
2021-11-04 02:16:17 -0300

    Ajustando a Home

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+const routes: Routes = [
+
+  {path: '', component: HomeComponent},
+  {path:'entrar',component: EntrarComponent},
+  {path:'cadastrar',component: CadastrarComponent},
+  {path: 'home', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
