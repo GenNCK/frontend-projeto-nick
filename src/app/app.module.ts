5f5a03ab4be3bb2a332d7e455428f926052f1fb1
beaavelino <beatrizavelino0088@gmail.com>
2021-11-18 23:56:41 -0300

    Código atualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component'
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { MenuForumComponent } from './menu-forum/menu-forum.component';
 
 
 
@@ -41,6 +42,7 @@ import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delet
     SobreNosComponent,
     PostagemEditComponent,
     PostagemDeleteComponent,
+    MenuForumComponent,
    
     
   ],
---
+++ src/app/menu-forum/menu-forum.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-forum',
+  templateUrl: './menu-forum.component.html',
+  styleUrls: ['./menu-forum.component.css']
+})
+export class MenuForumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
